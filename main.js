e19895e1d6622b6150f2c6ca3b2a286fa1d5d7d8
musicprogram <juan-o-henao@hotmail.es>
2019-07-11 12:20:28 -0500

    importando datos.json

--- main.js
+++ main.js
@@ -60,16 +60,96 @@ const graficar = () =>{
 
 
 
-const datos = [1,20,30,40,50,16,78,18,90,130]
+/*const datos = [1,20,30,40,50,16,78,18,90,130]
 const graficar = () =>{
 	d3.select('.miGrafica')
 		.selectAll('div')
 		.data(datos)
 		.enter().append('div')
 		.attr('class', 'barras')
-		.style('height', (d)=>{
+		.style('height', (d) =>{
 			return d + 'px'
 		})
 	
 
+}*/
+
+
+/*var datos = [1,2,5,6,20,30,80,120,150,240,300]
+
+
+const graficar = ()=>{
+	const svg = d3.select('body')
+								.append('svg')
+	const circulo = svg.selectAll('circle')
+											.data(datos)
+											.enter().append('circle')	
+
+	circulo.attr('cx',(d,i)=>{
+		return (i * 80) + 25;
+	})
+	.attr('cy', 300 / 2)
+	.attr('r',(d)=>{
+		return d 
+	})
+
+}*/
+
+
+let datos = [];
+
+
+const cargarDatos = () => {
+	d3.json('./datos.json')
+		.then((data)=> {
+			datos = data;
+			console.log(datos);
+			graficar()
+		})
+
+}
+
+const graficar = () => {
+
+	const w = 500;
+	const h = 300;
+
+
+
+	const svg = d3.select('body')
+								.append('svg')
+								.attr('width', w)
+								.attr('height', h);
+
+				svg.selectAll('rect')
+						.data(datos)
+						.enter()
+						.append('rect')
+						.attr('x',0)
+						.attr('y',0)
+						.attr('width', 20)
+						.attr('height', 100)
+						.attr('x',(d,i)=>{
+							return i * 40 + 30
+						})
+						.attr('height',(d)=>{
+							return d.dato;
+						})
+						.attr('y',(d)=>{
+							return h - d.dato;
+						})
+
+				svg.selectAll('text')
+						.data(datos)
+						.enter()
+						.append('text')
+						.text((d)=>{
+							return d.name;
+						})
+						.attr('x',(d,i)=>{
+							return i * 40 + 40
+						})	
+						.attr('y',(d)=>{
+							return h - d.dato - 3;
+						})	
 }
